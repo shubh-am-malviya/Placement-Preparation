06031997168758489b1bcc4a7d292084ebbd678c
Shubham Malviya <37037423+shubh-am-malviya@users.noreply.github.com>
2021-03-04 14:04:59 +0530

    Max sum subarray | Kadane algo

---
+++ CrackerGFG/Arrays/B7_kadane.cpp
@@ -0,0 +1,19 @@
+int maxSubarraySum(int arr[], int n){
+        
+    int maxEndingHere = 0, maxSoFar = 0;
+
+    for (int i = 0; i < n; i++) {
+        maxEndingHere += arr[i];
+
+        if (maxEndingHere < 0) {
+            maxEndingHere = 0;
+        }
+
+        if (maxSoFar < maxEndingHere) {
+            maxSoFar = maxEndingHere;
+        }
+    }
+
+    return maxSoFar;
+
+}
